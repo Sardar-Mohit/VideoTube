6ef6dd49027a3bb992791ebafe47569c3385ab9a
Sardar-Mohit <maherwanlundwani777@gmail.com>
2026-09-22 18:29:35 +0530

    Update CORS configuration to use environment variable for origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,12 +5,12 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN, 
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
