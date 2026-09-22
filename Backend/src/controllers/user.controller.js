58af51d7b20551e7cd9e58a89400861d48551ff4
Sardar-Mohit <maherwanlundwani777@gmail.com>
2026-09-22 19:39:31 +0530

    cookie changes

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -155,6 +155,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
   };
 
   return res
@@ -191,6 +192,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: "none",
     };
 
     return res
@@ -241,6 +243,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: "none",
     };
 
     const { accessToken, newRefreshToken } =
